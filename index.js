feff6090497aa04b455380ea6705a49c4720689e
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-03-16 22:29:46 +0545

    Fix linter file

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 console.log('hello roshan');
 window.addEventListener('DOMContentLoaded', () => {
-const btn = document.querySelector('#menu_btn');
-const menu = document.querySelector('#mobile-nav');
-const close = document.querySelector('#mobile-nav span');
-const list = document.querySelectorAll('#mobile-nav ul li');
+  const btn = document.querySelector('#menu_btn');
+  const menu = document.querySelector('#mobile-nav');
+  const close = document.querySelector('#mobile-nav span');
+  const list = document.querySelectorAll('#mobile-nav ul li');
 
-btn.addEventListener('click', () => {
+  btn.addEventListener('click', () => {
     menu.style.display = 'flex';
-})
-close.addEventListener('click', () => {
+  });
+  close.addEventListener('click', () => {
     menu.style.display = 'block';
-  })
-  list.forEach(li=> {
+  });
+  list.forEach((li) => {
     li.addEventListener('click', () => {
       menu.style.display = 'none';
-    })
-  })  
+    });
+  });
 });
